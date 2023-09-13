9874ea71895557551f042ac8a8b77a0693164d67
Jeffrey van Helden <jeffrey.van.helden@nzta.govt.nz>
2023-09-13 14:44:16 +1200

    bit more

--- src/main/java/guru/bonacci/camel/aggregator/SimpleRouteBuilder.java
+++ src/main/java/guru/bonacci/camel/aggregator/SimpleRouteBuilder.java
@@ -1,5 +1,7 @@
 package guru.bonacci.camel.aggregator;
 
+import java.awt.Choice;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,15 +35,27 @@ public class SimpleRouteBuilder extends RouteBuilder {
 		from("timer://foo?fixedRate=true&period=500")
 			.bean(Randomizer.class)
 			.setHeader("id", simple("${random(1,5)}"))
+			.to("direct:aggregate");
+
+		from("timer://foo?fixedRate=true&period=5000")
 			.setHeader("flush", simple("${random(1,5)}"))
+			.setHeader("id", header("flush"))
+			.setBody(constant("flushed!"))
+			.to("direct:aggregate");
+
+	  from("direct:aggregate")
 			.aggregate(header("id"), AggregationStrategies.bean(BodyAppender.class, "append"))
 				.completionSize(3)
 				.completionTimeout(8000)
-				.completionPredicate(header("flush").isEqualTo("1"))
-				.aggregateController(controller)
-			.to("direct:printer");
-		
-	  from("direct:printer")
-		  .log("Message received : ${headers.id}<>${body}");
+				.completionPredicate(header("flush").isNotNull())
+				.aggregateController(controller)	
+				.to("direct:print");
+
+	  from("direct:print")
+	  	.choice()
+		  	.when(body().isInstanceOf(List.class))	
+			  	.log("Message received : ${headers.id}<>${body}")
+	  		.otherwise()
+	  			.log("Houston : ${headers.id}<>${body}");
 	}
 }
--- src/main/java/guru/bonacci/camel/aggregator/beans/BodyAppender.java
+++ src/main/java/guru/bonacci/camel/aggregator/beans/BodyAppender.java
@@ -1,8 +1,11 @@
 package guru.bonacci.camel.aggregator.beans;
 
+import java.util.List;
+
 public class BodyAppender {
 
-	 public String append(String existing, String next) {
-     return next + "|" + existing;
+	 public List<String> append(List<String> existing, String next) {
+     existing.add(next);
+     return existing;
  }
 }
